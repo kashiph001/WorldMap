ac4733c8ece042190432321216f1053f15ca50fc
kashiph001 <126757362+kashiph001@users.noreply.github.com>
2023-06-17 22:15:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MapComponent from "./components/MapComponent";
+
+const App = () => {
+  return (
+    <div style={{ backgroundColor: "grey" }}>
+      {/* Render the MapComponent */}
+      <MapComponent />
+    </div>
+  );
+};
+
+export default App;
